1e7d9263e60ae786ea5f13e570bed16bd40d4dc1
piotr <piotr@DESKTOP-11VQ56L>
2023-01-23 19:34:24 +0100

    Dodaj pliki projektów.

---
+++ Controllers/FilmController.cs
@@ -0,0 +1,93 @@
+﻿using FilmDB502v2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB502v2.Controllers
+{
+    public class FilmController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            var manager = new FilmManager();
+            var films = manager.GetFilms();
+            return View(films);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(FilmModel film)
+        {
+            var manager = new FilmManager();
+            manager.AddFilm(film);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Remove(int id)
+        {
+            var manager = new FilmManager();
+            var film = manager.GetFilm(id);
+            return View(film);
+        }
+
+        [HttpPost]
+        public IActionResult RemoveConfirm(int id)
+        {
+            var manager = new FilmManager();
+            try
+            {
+                var film = manager.GetFilm(id);
+                manager.RemoveFilm(film.ID);
+                return RedirectToAction("Index");
+            }
+
+            catch (SystemException)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var manager = new FilmManager();
+            var film = manager.GetFilm(id);
+            return View(film);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(FilmModel filmModel)
+        {
+            var manager = new FilmManager();
+            try
+            {
+                var film = manager.UpdateFilm(filmModel);
+                return RedirectToAction("Index");
+            }
+
+            catch (SystemException)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id) 
+        {
+            var manager = new FilmManager();
+            var film = manager.GetFilm(id);
+            return View(film);
+        }
+    }
+}
---
+++ FilmManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using FilmDB502v2.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB502v2
+{
+    public class FilmManager
+    {
+        public FilmManager AddFilm(FilmModel filmModel)
+        {
+            using (var context = new FilmContext())
+            {
+                try
+                {
+                    context.Films.Add(filmModel);
+                    context.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    filmModel.ID = 0;
+                    context.Films.Add(filmModel);
+                    context.SaveChanges();
+                }
+            }
+            
+            return this;
+        }
+
+        public FilmManager RemoveFilm(int id)
+        {
+            using (var context = new FilmContext())
+            {
+                var wpis = context.Films.SingleOrDefault(x => x.ID == id);
+                context.Films.Remove(wpis);
+
+                context.SaveChanges();
+            }
+            return this;
+        }
+
+        public FilmManager UpdateFilm(FilmModel filmModel)
+        {
+            using (var context = new FilmContext())
+            {
+                var wpis = context.Films.Update(filmModel);
+                context.SaveChanges();
+            }
+
+            return this;
+        }
+
+        public FilmManager ChangeTitle(int id, string newTitle)
+        {
+            using (var context = new FilmContext())
+            {
+                var wpis = context.Films.Single(x => x.ID == id);
+                wpis.Title = newTitle;
+                context.SaveChanges();
+            }
+            return this;
+        }
+
+        public FilmModel GetFilm(int id)
+        {
+            using (var context = new FilmContext())
+            {
+                var wpis = context.Films.SingleOrDefault(x => x.ID == id);
+                return wpis;
+            }
+        }
+
+        public List<FilmModel> GetFilms()
+        {
+            using (var context = new FilmContext())
+            {
+                return context.Films.ToList<FilmModel>();
+            }
+        }
+    }
+}
